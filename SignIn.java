99d62ee15da9ec8700876fb5c61b0551830b8ed9
ls <ls@ls.com>
2020-11-30 17:25:50 +0800

    彻底解决冲突

--- SignIn.java
+++ SignIn.java
@@ -1,14 +1,10 @@
 public class SignIn{
 	private int id;
-<<<<<<< HEAD
+
 	private String name;
 
 	public void sign(){
-
-=======
-
-	public void sign(){
 		System.out.println("id - " + id);
->>>>>>> 6d85ae8e9d4b6f346c90163f104a5fbebf699822
+
 	}
 }
